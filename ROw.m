fd3b5844443d6e2e3fe280c1cd9929e0edc7bd2f
kristina-sarovic <82336657+kristina-sarovic@users.noreply.github.com>
2021-04-11 22:32:09 +0200

    Update ROw.m

--- ROw.m
+++ ROw.m
@@ -1,5 +1,9 @@
 function ro = ROw(T)
-    % T     C       temperatura vode
-    % ro    kg/m3   gustoca vode 
-    ro = (1-1.9549*10^(-5)*abs(T+273.15-277).^1.68)*10^3; % kg/m3 -  gustoca vode 
+  % This function calculates the water density as a function of its temperature.
+    %% --- INPUT ----------------------------------------------------------
+        % T 	% Water temperature 	[ C ]
+    %% --- OUTPUT ---------------------------------------------------------
+        % ro    % Water density         [ kg m-3 ]
+    %% --- CALCULATION ----------------------------------------------------        
+    ro = (1-1.9549*10^(-5)*abs(T+273.15-277).^1.68)*10^3;
 end
